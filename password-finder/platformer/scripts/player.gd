be855003c1b647d8df6a3d28fd8c4254b551d9c1
Antonio-Solito <asolito1@nait.ca>
2024-01-06 15:03:12 -0700

    Created player sprite, added movement script

---
+++ password-finder/platformer/scripts/player.gd
@@ -0,0 +1,8 @@
+extends CharacterBody2D
+
+@export var speed = 300
+
+func _physics_process(delta):
+	var horizontal_direction = Input.get_axis("move_left", "move_right")
+	velocity.x = speed * horizontal_direction	
+	move_and_slide()

55eba6edbbce0ee66f956281166e1aeb3000e2bb
SeraphelCelaena <67750475+SeraphelCelaena@users.noreply.github.com>
2024-01-06 23:13:32 -0700

    tryin to do things but not working as intended

--- password-finder/wordle/wordle.cs
+++ password-finder/wordle/wordle.cs
@@ -9,16 +9,22 @@ public partial class wordle : Control
 	[Export]
 	public PackedScene VBoxContainerScene {get;set;}
 
-	private List<string> _passwordSplit = new List<String>();
+	private List<string> _passwordSplit = new List<String>() {"Banana", "Dog", "Pizza"};
+	private List<VBoxContainer> _letterContainers = new List<VBoxContainer>();
+	private LineEdit textField;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		HBoxContainer hBoxContainer = GetNode<HBoxContainer>("guesses");
+		textField = GetNode<LineEdit>("TextField");
+		textField.MaxLength = _passwordSplit.Count;
 		for (int i = 0; i < _passwordSplit.Count; i++)
 		{
 			VBoxContainer vBoxContainer = (VBoxContainer)VBoxContainerScene.Instantiate();
+			_letterContainers.Add(vBoxContainer);
 			hBoxContainer.AddChild(vBoxContainer);
+			vBoxContainer.GetNode<Label>("%Label").Text = "";
 		}
 	}
 
@@ -30,5 +36,16 @@ public partial class wordle : Control
 	public void GetPassword(string[] passwordSplit)
 	{
 		_passwordSplit = new List<string>(passwordSplit);
+	}
+
+	private void OnTextFieldChanged(string newText)
+	{
+		GD.Print(newText);
+		_letterContainers[newText.Length - 1].GetNode<Label>("%Label").Text = newText[newText.Length - 1].ToString();
+	}
+
+	private void OnTextFieldSubmit(string newText)
+	{
+		GD.Print(newText);
 	}
 }

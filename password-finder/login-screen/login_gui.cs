8a9152d13fced6eaaeed747442cbaf612146cbce
SeraphelCelaena <67750475+SeraphelCelaena@users.noreply.github.com>
2024-01-06 19:19:48 -0700

    i can take stuphs from all.json now

--- password-finder/login-screen/login_gui.cs
+++ password-finder/login-screen/login_gui.cs
@@ -1,6 +1,7 @@
 using Godot;
 using System;
 using System.Collections.Generic;
+using System.Text;
 
 public partial class login_gui : Control
 {
@@ -10,9 +11,16 @@ public partial class login_gui : Control
 	private const string _USER_NAME = "HackED-Participant";
 	private string password = String.Empty;
 
+	private List<string> wordList = new List<string>();
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		GetWordList();
+		for (int i = 0; i < wordList.Count; i++)
+		{
+			GD.Print(wordList[i]);
+		}
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -23,5 +31,19 @@ public partial class login_gui : Control
 	private void GeneratePassword()
 	{
 		
+	}
+
+	private void GetWordList()
+	{
+		using var file = FileAccess.Open("res://assets/all.json", FileAccess.ModeFlags.Read);
+		string content = file.GetAsText();
+		List<string> tempList = new List<string>(content[2..^2].Split("\",\""));
+		for (int i = 0; i < tempList.Count; i++)
+		{
+			if (tempList[i].Length == 5)
+			{
+				wordList.Add(tempList[i]);
+			}
+		}
 	}
 }

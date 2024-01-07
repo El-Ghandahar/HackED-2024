d17a015565dbc581ece3b05bf15e0147c2bc0b32
SeraphelCelaena <67750475+SeraphelCelaena@users.noreply.github.com>
2024-01-06 17:20:36 -0700

    figuring out how to make this work

--- password-finder/login-screen/login_gui.cs
+++ password-finder/login-screen/login_gui.cs
@@ -1,9 +1,15 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class login_gui : Control
 {
-	string USER_NAME = "HackED-Participant";
+	[Signal]
+	public delegate void PasswordEventHandler(string password);
+
+	private const string _USER_NAME = "HackED-Participant";
+	private string password = String.Empty;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -12,5 +18,10 @@ public partial class login_gui : Control
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+
+	private void GeneratePassword()
+	{
+		
 	}
 }
